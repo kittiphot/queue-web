50d089f938b5eb7c4e899e34237cc50e27ddf7d8
Ikkew-san <ekkarat.no@gmail.com>
2018-05-03 10:12:03 +0700

    .

--- src/app/Screen/Screen.module.ts
+++ src/app/Screen/Screen.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 import { ScreenRoutingModule } from './Screen-routing.module';
 import { ScreenComponent } from './Screen.component';
 import { QueueService } from '../shared/services/queue.service'
 import { SettingsService } from '../shared/services/settings.service'
 import { DateTimeService } from '../shared/services/datetime.service'
+import { QueueComponent } from '../queue/queue.component';
 
 @NgModule({
-    imports: [CommonModule, ScreenRoutingModule],
-    declarations: [ScreenComponent],
+    imports: [CommonModule, ScreenRoutingModule,NgxQRCodeModule],
+    declarations: [ScreenComponent,QueueComponent],
     providers: [
         QueueService,
         SettingsService,
